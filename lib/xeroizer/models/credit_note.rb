17a060966fa448600658f297ebfee8b7d1119745
Robert Rouse <robert@kickserv.com>
2016-08-22 10:19:05 -0500

    Change fully_paid_on_date in CreditNote from a datetime to a date since there is no time

--- lib/xeroizer/models/credit_note.rb
+++ lib/xeroizer/models/credit_note.rb
@@ -58,7 +58,7 @@ module Xeroizer
       datetime_utc  :updated_date_utc, :api_name => 'UpdatedDateUTC'
       string        :currency_code
       decimal       :currency_rate
-      datetime      :fully_paid_on_date
+      date          :fully_paid_on_date
       boolean       :sent_to_contact
       decimal :remaining_credit
 
