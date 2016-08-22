2c7dce3c5220ba40aaa2afac9f443e7d26be0733
Robert Rouse <robert@kickserv.com>
2016-08-22 10:19:31 -0500

    Change fully_paid_on_date in Invoice from a datetime to a date since there is no time Add UpdatedDateUtc to Item

--- lib/xeroizer/models/invoice.rb
+++ lib/xeroizer/models/invoice.rb
@@ -75,7 +75,7 @@ module Xeroizer
       datetime_utc :updated_date_utc, :api_name => 'UpdatedDateUTC'
       string       :currency_code
       decimal      :currency_rate
-      datetime     :fully_paid_on_date
+      date         :fully_paid_on_date
       datetime     :expected_payment_date
       boolean      :sent_to_contact
       boolean      :has_attachments
